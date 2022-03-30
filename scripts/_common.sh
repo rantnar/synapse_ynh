f2b936c15e79de9487e0394a87887e04124adef1
rantnar <34136645+rantnar@users.noreply.github.com>
2022-03-30 15:27:08 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -48,7 +48,7 @@ install_sources() {
         pip3 install --upgrade setuptools wheel pip
         chown $synapse_user:root -R $final_path
         sudo -u $synapse_user env PATH=$PATH pip3 install --upgrade 'cryptography>=3.4.7'
-        pip3 install --upgrade cffi ndg-httpsclient psycopg2 lxml jinja2
+        pip3 install --upgrade cffi ndg-httpsclient psycopg2 lxml jinja2==3.0.1
         # Fix issue https://github.com/YunoHost-Apps/synapse_ynh/issues/248
         pip3 install --upgrade 'Twisted>=21' 'treq>=21.1.0' matrix-synapse==$upstream_version matrix-synapse-ldap3
 
